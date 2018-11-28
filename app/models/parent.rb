cd04bbd99c48575b040f79444d3a2a16e3a62ef2
ChunPanYung <>
2018-11-27 22:04:47 -0500

    fix error on parent.rb

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -1,5 +1,5 @@
 class Parent < ApplicationRecord
   has_many :child
-  validates :user, presence: true, length: { maximum: 40 }
+  validates :name, presence: true, length: { maximum: 40 }
   validates :password, presence: true
 end
