22db7dfc06dfccc0851f6ff0c258ef85e94113dc
ChunPanYung <>
2018-11-27 21:12:05 -0500

    change to routes, add /signup to url

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :children
-  resources :parents
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # resources :children
+  # resources :parents
+  get '/signup', to: 'parents#new'
 end
