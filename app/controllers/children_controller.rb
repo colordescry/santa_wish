3ad26b0c76b0245ae6e272cf940847a10dfed1f7
ChunPanYung <>
2018-12-01 18:54:57 -0500

    Able to use ajax for deleting items in child's wishlist.
    WIP: search function with ajax

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -64,6 +64,16 @@ class ChildrenController < ApplicationController
     end
   end
 
+  def search
+    name = params[search] + '%'
+    id = session[:current_child_id]
+    @wishlist= Wishlist.where(['name LIKE ? AND child_id = ?', name, id])
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_child
--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,10 @@ Rails.application.routes.draw do
   post   '/login',  to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
 
+
   get '/new_child', to: 'children#new'
   post '/new_child', to: 'children#new'
+  get '/child_search', to: 'child#search'
 
 
 
